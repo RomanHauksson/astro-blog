bd566bc1ce92403901d01ba623c4ad8ded63b062
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-02-28 12:50:28 -0800

    Update site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Roman Hauksson';
+export const SITE_DESCRIPTION = "Roman Hauksson's website";
