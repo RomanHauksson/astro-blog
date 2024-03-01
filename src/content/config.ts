1f5cc0fcd228652c6f0b61eac1b8b1afe56b74f4
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-01 10:55:57 -0800

    Add LEBRON project

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+    heroImage: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { blog: blogCollection };
+
