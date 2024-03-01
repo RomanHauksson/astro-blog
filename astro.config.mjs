14e9c749efc77a3a96357c649c278d0590b8d709
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-01 00:43:51 -0800

    Too lazy to label this commit rn. I'm done with the MVP now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), icon(), preact()]
 });
