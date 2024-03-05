d1fb17689caaf5567918cfb4df33b834979f947e
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-04 20:31:06 -0800

    Add math support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import preact from "@astrojs/preact";
-
+import remarkMath from "remark-math"
+import rehypeKatex from 'rehype-katex';
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon(), preact(), partytown()]
+  integrations: [mdx(), sitemap(), tailwind(), icon(), preact(), partytown()],
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
