5417f6a2e0f045cde232c5d75c8b513b00c2c882
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-09 15:39:17 -0800

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import remarkMath from "remark-math"
+import remarkMath from "remark-math";
 import rehypeKatex from 'rehype-katex';
 import partytown from "@astrojs/partytown";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind(), icon(), partytown(), react()],
   markdown: {
     // Applied to .md and .mdx files
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+    rehypePlugins: [rehypeKatex]
+  }
 });
