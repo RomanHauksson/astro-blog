f06f617fffbc6b7cc751108cee66800ca5e0681e
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-09 15:35:30 -0800

    Remove Preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import preact from "@astrojs/preact";
 import remarkMath from "remark-math"
 import rehypeKatex from 'rehype-katex';
 import partytown from "@astrojs/partytown";
@@ -11,7 +10,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon(), preact(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind(), icon(), partytown()],
   markdown: {
     // Applied to .md and .mdx files
     remarkPlugins: [remarkMath],
