e1f78063f3def32b6b742a17c6533c1faa1d957b
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-03-09 20:48:41 -0800

    Add Roman splat

---
+++ src/components/Roman.jsx
@@ -0,0 +1,21 @@
+import { Canvas } from "@react-three/fiber";
+import { Splat, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { Icon } from '@iconify/react';
+
+const Roman = () => {
+  return (
+    <div className="relative h-[36rem] w-full rounded-lg m-auto border-2 border-slate-800">
+      <Icon className="absolute top-0 right-0 m-4 h-8 w-8 text-slate-300" icon="material-symbols:3d-rotation" />
+      <Canvas camera={{ position: [-1.7, 0.6, 0] }}>
+        <OrbitControls />
+        <Splat
+          src="/Roman.splat"
+          rotation={[0 * Math.PI, 0 * Math.PI, 0 * Math.PI]}
+        />
+      </Canvas>
+    </div>
+  );
+};
+
+export default Roman;
