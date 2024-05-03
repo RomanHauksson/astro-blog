9b1d0c6b97c2473a30cf201b3dc40f9d3e163d20
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-05-02 18:06:25 -0700

    Many updates

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				flash: {
+					'from': { 'background-color': 'rgba(255, 255, 255, 0.2)' },
+					'to': { 'background-color': 'transparent' }
+				}
+			},
+			animation: {
+				flash: 'flash 1s'
+			}
+		},
 	},
 	plugins: [require('@tailwindcss/typography'),],
 }
