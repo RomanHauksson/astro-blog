9f387cfa5bcb47fd3d5ef6ee58432bdab990e614
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-07-05 13:02:12 -0700

    Update resume

---
+++ client/src/types/resume.d.ts
@@ -0,0 +1,35 @@
+type SectionData = {
+  name: string;
+  icon?: string;
+  type: "organizations" | "items";
+  entries: OrganizationData[] | ItemData[];
+}
+
+type OrganizationData = {
+  name: string;
+  location?: string;
+  link?: string;
+  positions: PositionData[];
+}
+
+type PositionData = {
+  title: string;
+  link?: string;
+  startMonth: string;
+  endMonth: string;
+  location?: string;
+  notes?: string[];
+  technologies?: string[];
+}
+
+type ItemData = {
+  name: string;
+  location?: string;
+  link?: string;
+  startMonth?: string;
+  endMonth?: string;
+  notes?: string[];
+  technologies?: string[];
+}
+
+export { SectionData, OrganizationData, PositionData, ItemData };
