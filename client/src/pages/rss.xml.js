89fcd8c8d7ea752dbf7c7e49939a8a30de4b1ccd
Roman Hauksson <62364808+RomanHauksson@users.noreply.github.com>
2024-07-06 10:56:43 -0700

    Fix RSS pubDate

--- client/src/pages/rss.xml.js
+++ client/src/pages/rss.xml.js
@@ -11,6 +11,7 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
+			pubDate: new Date(post.data.publishDate),
 		})),
 	});
 }
